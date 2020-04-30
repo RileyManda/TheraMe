d38f0b0f60ddb35b473321c6de2cf35ed406b893
rileymanda1@gmail.com <rileymanda1@gmail.com>
2020-04-30 06:18:08 +0530

    push all

---
+++ app/src/main/java/com/smartsapien/therame/SplashActivity.java
@@ -0,0 +1,54 @@
+package com.smartsapien.therame;
+
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
+
+public class SplashActivity extends AwesomeSplash {
+
+    @Override
+    public void initSplash(ConfigSplash configSplash) {
+        configSplash.setBackgroundColor(R.color.colorPrimary); //colors.xml
+        configSplash.setAnimCircularRevealDuration(2000); //int// ms
+        configSplash.setRevealFlagX(Flags.REVEAL_LEFT);  //or Flags.REVEAL_LEFT
+        configSplash.setRevealFlagY(Flags.REVEAL_TOP); //or Flags.REVEAL_TOP
+        //Customize Logo
+        configSplash.setLogoSplash(R.mipmap.ic_launcher); //or any other drawable
+        configSplash.setAnimLogoSplashDuration(2000); //int ms
+        configSplash.setAnimLogoSplashTechnique(Techniques.Bounce); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
+
+
+        //Customize Path
+        // configSplash.setPathSplash(SyncStateContract.Constants.DROID_LOGO); //set path String
+        configSplash.setOriginalHeight(400); //in relation to your svg (path) resource
+        configSplash.setOriginalWidth(400); //in relation to your svg (path) resource
+        configSplash.setAnimPathStrokeDrawingDuration(3000);
+        configSplash.setPathSplashStrokeSize(3); //I advise value be <5
+        configSplash.setPathSplashStrokeColor(R.color.colorAccent); //any color you want form colors.xml
+        configSplash.setAnimPathFillingDuration(3000);
+        configSplash.setPathSplashFillColor(R.color.colorPrimaryDark); //path object filling color
+
+
+        // Title
+        configSplash.setTitleSplash("TheraMe"); //change your app name here
+        configSplash.setTitleTextColor(R.color.white);
+        configSplash.setTitleTextSize(30f); //float value
+        configSplash.setAnimTitleDuration(3000);
+        configSplash.setAnimTitleTechnique(Techniques.FlipInX);
+//        configSplash.setTitleFont("fonts/Pacifico.ttf"); //provide string to your font located in assets/fonts/
+
+    }
+
+    @Override
+    public void animationsFinished() {
+        Toast.makeText(SplashActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
+        Intent goHome = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(goHome);
+        //transit to another activity here
+        //or do whatever you want
+    }
+}
