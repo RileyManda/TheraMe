e29741552f0a58582eecbcf4a36d6cc72ad9a9a3
rileymanda1@gmail.com <rileymanda1@gmail.com>
2020-05-05 04:44:29 +0530

    updated profile|created onboarding view

--- app/src/main/java/com/smartsapien/therame/MainActivity.java
+++ app/src/main/java/com/smartsapien/therame/MainActivity.java
@@ -7,21 +7,66 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.smartsapien.therame.auth.LoginActivity;
 import com.smartsapien.therame.profile.ProfileActivity;
+import com.smartsapien.therame.utils.Utils;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.fabchat)FloatingActionButton fabChat;
+    @BindView(R.id.chatclose)FloatingActionButton fabClose;
+
+    public static final String PREF_USER_FIRST_TIME = "user_first_time";
+    boolean isUserFirstTime;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
          setContentView(R.layout.activity_main);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        ButterKnife.bind(this);
+
+
+        isUserFirstTime = Boolean.parseBoolean(Utils.readSharedSetting(MainActivity.this, PREF_USER_FIRST_TIME, "true"));
+
+        Intent introIntent = new Intent(MainActivity.this, OnBoardActivity.class);
+        introIntent.putExtra(PREF_USER_FIRST_TIME, isUserFirstTime);
+
+        if (isUserFirstTime)
+            startActivity(introIntent);
+        FloatingActionButton fab = findViewById(R.id.fabchat);
+
+
+        fabChat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                fabChat.setVisibility(View.GONE);
+                fabClose.setVisibility(View.VISIBLE);
+
+
                 Snackbar.make(view, "Chat with TheraBot", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        fabClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fabChat.setVisibility(View.VISIBLE);
+                fabClose.setVisibility(View.GONE);
+
+
+                Snackbar.make(view, "Speak to TheraMe", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
